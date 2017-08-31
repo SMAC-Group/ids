2b9a65f6825a139f0db1c526df08a6f08bf0f00e
Justin Lee <munsheet93@gmail.com>
2017-08-31 16:02:35 -0400

    add documentation for update stat 297 + add R file for stat297 package + update NAMESPACE

---
+++ R/stat297-package.R
@@ -0,0 +1,6 @@
+#' wv
+#' @keywords internal
+#' @import devtools knitr rmarkdown shiny
+#' @name stat297
+#' @docType package
+NULL
--- R/update.R
+++ R/update.R
@@ -1,3 +1,8 @@
+#' @title Update stat297 package
+#' @description Update the most recent stat297 package.
+#' @return An updated version of the stat297 package.
+#' @example
+#' update_stat297()
 #' @export
 update_stat297 = function(){
   devtools::install_github("SMAC-Group/stat297")
